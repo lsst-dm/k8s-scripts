b2e13636a84d3e00c7160589e5b33e1397b1adb5
Stephen R. Pietrowicz <srp@ncsa.illinois.edu>
2017-08-11 14:16:37 -0500

    Initial install script (part1) hosts file used by install part1 script redirect YML for port-forwarding local registry YML

---
+++ bin/install_part1.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+
+#
+# initial lifting for installing kubernetes - refer to README.md for additional steps required after this first part of the install is completed
+#
+
+#
+# system software update
+#
+yum update -y
+
+#
+# install kubernetes repo
+#
+cat << EOF >/etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+#
+# Add the Docker public key
+#
+rpm --import "https://sks-keyservers.net/pks/lookup?op=get&search=0xee6d536cf7dc86e2d7d56f59a178ac6c6238f52e"
+
+#
+# Install the Docker repo
+#
+yum-config-manager --add-repo https://packages.docker.com/1.12/yum/repo/main/centos/7
+
+#
+# Install docker-engine 1.12
+#
+yum install -y docker-engine
+
+#
+# Add centos as a user of docker
+#
+usermod -aG docker centos
+
+#
+# Turn off enforcement
+#
+setenforce 0
+
+#
+# Install 1.70 of kubernetes utils.
+# (Note that the current version (as of this writing) 1.71, has a bug which
+# prevents this install from happening.
+#
+yum install -y kubelet-1.7.0 kubeadm-1.7.0 kubernetes-cni-1.7.0 kubectl-1.7.0
+
+#
+# add hosts
+#
+cp /etc/hosts /etc/hosts.bak
+cat etc/newhosts.txt >>/etc/hosts
+
+#
+# enable and start docker
+#
+systemctl enable docker
+systemctl start docker
+
+
+#
+# change systemd to cgroupfs
+#
+cp /etc/systemd/system/kubelet.service.d/10-kubeadm.conf /etc/systemd/system/kubelet.service.d/10-kubeadm.conf.bak
+
+sed 's/KUBELET_CGROUP_ARGS=--cgroup-driver=systemd/KUBELET_CGROUP_ARGS=--cgroup-driver=cgroupfs/' < /etc/systemd/system/kubelet.service.d/10-kubeadm.conf.bak > /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+
+#
+# enable and start kubelet
+#
+systemctl enable kubelet
+systemctl start kubelet
+
+
+#
+# set iptables
+#
+echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
