995466dc0413b078433f7ee616ba65710b2d3b5c
Stephen Pietrowicz <srp@lsst-elast-node13.ncsa.illinois.edu>
2018-01-23 15:45:40 -0600

    New prepare_vm.sh script.  Installs latest k8s.

--- bin/prepare_vm.sh
+++ bin/prepare_vm.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
 #
-# add hosts
-#
-#cp /etc/hosts /etc/hosts.bak
-#cat etc/newhosts.txt >>/etc/hosts
-
-
-#
 # initial lifting for installing kubernetes - refer to README.md for additional steps required after this first part of the install is completed
 #
 
@@ -15,6 +8,13 @@
 # system software update
 #
 yum update -y
+
+
+#
+# Install docker
+#
+yum install -y docker
+systemctl enable docker && systemctl start docker
 
 #
 # install kubernetes repo
@@ -31,26 +31,6 @@ gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
 EOF
 
 #
-# Add the Docker public key
-#
-rpm --import "https://sks-keyservers.net/pks/lookup?op=get&search=0xee6d536cf7dc86e2d7d56f59a178ac6c6238f52e"
-
-#
-# Install the Docker repo
-#
-yum-config-manager --add-repo https://packages.docker.com/1.12/yum/repo/main/centos/7
-
-#
-# Install docker-ce
-#
-yum install -y docker
-
-#
-# Add centos as a user of docker
-#
-#usermod -aG docker centos
-
-#
 # Turn off enforcement
 #
 setenforce 0
@@ -58,24 +38,15 @@ setenforce 0
 #
 # Install kubernetes utils.
 #
-yum install -y kubelet-1.8.5
-yum install -y kubectl-1.8.5
-yum install -y kubeadm-1.8.5
+yum install -y kubelet kubeadm kubectl
 
-#
-# enable and start docker
-#
-systemctl enable docker
-systemctl start docker
 
+# add swap flag, since we're running on a system with swap enabled....
 
 #
 # change systemd to cgroupfs
 #
 KUBEADM_CONF=/etc/systemd/system/kubelet.service.d/10-kubeadm.conf
-cp /etc/systemd/system/kubelet.service.d/10-kubeadm.conf /etc/systemd/system/kubelet.service.d/10-kubeadm.conf.bak
-
-sed 's/KUBELET_CGROUP_ARGS=--cgroup-driver=systemd/KUBELET_CGROUP_ARGS=--cgroup-driver=cgroupfs/' < /etc/systemd/system/kubelet.service.d/10-kubeadm.conf.bak > $KUBEADM_CONF
 
 #
 # add option to turn off swap warning.
@@ -85,11 +56,14 @@ printf '%s\n' 2i 'Environment="KUBELET_EXTRA_ARGS=--fail-swap-on=false"' . x | e
 #
 # enable and start kubelet
 #
-systemctl enable kubelet
-systemctl start kubelet
-
+systemctl enable kubelet && systemctl start kubelet
 
 #
 # set iptables
 #
-echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
+cat <<EOF >  /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system
+
